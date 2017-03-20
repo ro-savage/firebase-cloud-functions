f6793331a99b9883db64acc2b19f22d1bdf07b84
Ro Savage <rowan.savage@gmail.com>
2017-03-20 23:18:10 +1100

    More funcs

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-require('@google-cloud/debug-agent').start();
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const fetch = require('node-fetch');
@@ -17,14 +16,20 @@ exports.reserveTicket = functions.https.onRequest((req, res) => {
   })
 });
 
+exports.reserveTicket2g = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const query = req.query;
+  // Push it into the Realtime Database then send a response
+  cors(req, res, () => {
+    admin.database().ref(`/tickets/${query.eventid}/tickets/${query.ticketid}`).update({available: false, reserved: true }).then(() => {
+      res.status(201).json({message: 'Ticket reserved', ticketId: query.ticketid, error: false });
+    });
+  })
+});
+
 exports.purchaseTicket = functions.https.onRequest((req, res) => {
 
   const query = req.query;
-
-  // fetch('https://us-central1-functions-56eb1.cloudfunctions.net/validatePayment')
-  //   .then(response => response.json())
-  //   .then(json => confirmedPurchase())
-  //
 
   const confirmedPurchase = () => {
     cors(req, res, () => {
@@ -56,5 +61,46 @@ exports.resetTicket = functions.https.onRequest((req, res) => {
     admin.database().ref(`/tickets/${query.eventid}/tickets/${query.ticketid}`).update({available: true, reserved: false, purchased: false }).then(() => {
       res.status(201).json({message: 'Ticket reset', ticketId: query.ticketid, error: false });
     })
+  })
+})
+
+exports.resetTicket2g = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const query = req.query;
+  // Push it into the Realtime Database then send a response
+  cors(req, res, () => {
+    admin.database().ref(`/tickets/${query.eventid}/tickets/${query.ticketid}`).update({available: true, reserved: false, purchased: false }).then(() => {
+      res.status(201).json({message: 'Ticket reset', ticketId: query.ticketid, error: false });
+    })
+  })
+})
+
+exports.superFastWithQuery = functions.https.onRequest((req, res) => {
+  res.status(200).json({id: req.query.ticketid });
+})
+
+exports.superFast = functions.https.onRequest((req, res) => {
+  res.json({fast: 'fast!' });
+})
+
+
+function calcFib(num) {
+  console.time('fibonacci');
+  const start = new Date()
+  const fib = fibonacci(num)
+  const end = new Date() - start
+  console.timeEnd('fibonacci');
+  return { num: num, answer: fib, time: end }
+}
+
+function fibonacci(num) {
+  if (num <= 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+exports.heavyLoad = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    res.json(calcFib(32))
   })
 })
